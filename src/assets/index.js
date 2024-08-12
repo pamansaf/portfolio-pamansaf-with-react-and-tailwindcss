a2e6d1691013629283e184870a08c523a7f05747
Safwan <safwan.ssy@gmail.com>
2024-08-13 00:10:24 +0700

    ubah letak gallery menjadi di assets>img

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,12 @@
 import logo from "./images/safwansy.jpg";
 import bannerImg from "./images/bannerImg.png";
+import desain1 from "./images/gallery/desain1.jpg";
+import desain2 from "./images/gallery/desain2.jpg";
+import desain3 from "./images/gallery/desain3.jpg";
+import desain4 from "./images/gallery/desain4.jpg";
+import desain5 from "./images/gallery/desain5.jpg";
+import desain6 from "./images/gallery/desain6.jpg";
+import imsaqiyah from "./images/gallery/imsakiyah.png";
 import project1 from "./images/projects/project1.png";
 import project2 from "./images/projects/project2.png";
 import project3 from "./images/projects/project3.png";
@@ -14,6 +21,13 @@ import contactImg from "./images/contact/contactImg.png";
 export {
   logo,
   bannerImg,
+  desain1,
+  desain2,
+  desain3,
+  desain4,
+  desain5,
+  desain6,
+  imsaqiyah,
   project1,
   project2,
   project3,
--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import Title from "../layouts/Title";
+import {
+  desain1,
+  desain2,
+  desain3,
+  desain4,
+  desain5,
+  desain6,
+  imsaqiyah,
+} from "../../assets/index";
 
 const Gallery = () => {
   return (
@@ -15,15 +24,14 @@ const Gallery = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-8 ">
         <div className="flex flex-col gap-10">
-          <img src="../public/desain1.jpg" alt="" />
-          <img src="../public/desain6.jpg" alt="" />
+          <img src={desain1} alt="" />
+          <img src={desain6} alt="" />
         </div>
-        <img src="../public/desain3.jpg" alt="" />
-        <img src="../public/desain5.jpg" alt="" />
-        <img src="../public/desain4.jpg" alt="" />
-
-        <img src="../public/desain2.jpg" alt="" />
-        <img src="../public/imsakiyah.png" alt="" />
+        <img src={desain3} alt="" />
+        <img src={desain5} alt="" />
+        <img src={desain4} alt="" />
+        <img src={desain2} alt="" />
+        <img src={imsaqiyah} alt="" />
       </div>
     </section>
   );
